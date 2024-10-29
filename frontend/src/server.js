8a78d43d21f6eae955065f9c5f895418d4988725
Anh Tu Nguyen <106453722+leo2971998@users.noreply.github.com>
2024-10-29 03:57:13 -0500

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -6,21 +6,7 @@ const bcrypt = require('bcrypt');
 require('dotenv').config();
 const app = express();
 const port = 5000; // Change to 6000 when you push to GitHub
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:3002',
-    'https://black-desert-0587dbd10.5.azurestaticapps.net'
-];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static('public')); // Allows access to the public folder for images
