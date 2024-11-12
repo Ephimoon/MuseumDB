1c5d6f0dc899da4f00eee6bb551a1865dfe90831
Anh Tu Nguyen <106453722+leo2971998@users.noreply.github.com>
2024-11-12 15:20:33 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
