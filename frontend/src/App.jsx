48d175bced9c6f90cc7f7acde6b5ce46bd76d20f
Anh Tu Nguyen <106453722+leo2971998@users.noreply.github.com>
2024-11-12 15:16:27 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import Visit from './pages/Visit';
@@ -37,6 +38,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <TicketCartProvider> {/* Wrapping Routes with TicketCartProvider */}
+                <ToastContainer />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/Visit" element={<Visit />} />
